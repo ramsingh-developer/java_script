504eb82c09b83e0f5b1aa894542f36561e9b3ecd
Ram Singh <singhram25122005@gmail.com>
2026-01-30 20:24:46 +0000

    data-type

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js, not browser
+
+console.log(3
+    +
+    3);  // code readability should be high
+
+console.log("Ram Singh");
+
+
+let name = 'Ram'
+let age = 20
+let isLoggedIn = false
+let state;
+
+/* PRIMITIVE DATA TYPES */
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// objest
+
+console.log(typeof undefined);  // output=> undefined 
+console.log(typeof null);    // output=> objest
