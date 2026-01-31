76423dd0869dbbcec054f1f8a57853cb7b686d5d
Ram Singh <singhram25122005@gmail.com>
2026-01-31 18:09:58 +0000

    datatype-summary

---
+++ 01_Basics/05_DataType-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;   // undefined output dega
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 37434343834384345n
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Funtions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let obj = {
+    name: "ram",
+    age:20,
+}
+
+const myFuntion = function(){
+        console.log("Hello World");
+}
+
+console.log(typeof heros);
