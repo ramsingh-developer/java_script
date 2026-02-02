9b2d75cc1b461b40a0b8a76c857d799d5d1fd5f2
Ram Singh <singhram25122005@gmail.com>
2026-02-02 19:13:05 +0000

    object2

--- 02_Basics/04_Object.js
+++ 02_Basics/04_Object.js
@@ -53,4 +53,4 @@ console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
---
+++ 02_Basics/05_Object.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename : 'js in hindi',
+    price: "999",
+    courseInstructor: 'Hitesh sir'
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "Hitesh",
+//     "coursename": "js in Hindi",
+//    " price": "free"
+// }
+
+
+
+[
+    {},
+    {},
+    {}
+]
