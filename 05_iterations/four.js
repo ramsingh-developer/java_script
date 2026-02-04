5fa2be89409b2599413ae275ec964e7fc85af24f
Ram Singh <singhram25122005@gmail.com>
2026-02-04 23:14:05 +0000

    simple-map-method

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);  
+    
+}
+
+const programming = ['js', "rb", "py", "java", "cpp"]
+
+for (const key in programming){
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United Sates of America")
+// map.set('Fe', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
