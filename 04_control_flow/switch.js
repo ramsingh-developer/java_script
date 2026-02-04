d9eb4014246cdba08ec2a9065513151fbf84fc87
Ram Singh <singhram25122005@gmail.com>
2026-02-04 19:38:02 +0000

    switch_case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {            // item select karke (shift+alt+down arrow) karne se selected item neeche paste hoga
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+
+// imp=> ak bar jaha pe bhi case match hongya uske bad ka sara ka sara code execute karega *exept default *
