e5e06e30834d3a0d5804c348659f6d43fafb7d54
Ram Singh <singhram25122005@gmail.com>
2026-02-04 10:37:33 +0000

    iife

---
+++ 03_Basics/05_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){                // pure function ko () me wrap kar dege
+    // named IIFE                  
+    console.log(`DB CONNECTED`);
+    
+})();         // do iife ko aksath likhne ke liye ; ka use karte hai
+
+( (name) => {
+    console.log(`DB CONNECTED two ${name}`);
+    
+})('ram singh')
+
+// iife ka use kab karte hai hai => global scope ke polusion se problem hoti hai kayi bar to us global scope ke jo variables hai uske polusion ko hatane ke liye ham IIfe ka use kiya
